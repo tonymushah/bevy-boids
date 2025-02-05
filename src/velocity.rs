8c521030ac26c2cb652c280906c9b44dc0e314d8
tonymushah <tonyramanantsalama@gmail.com>
2025-02-06 01:14:17 +0300

    velocity handle pause

--- src/velocity.rs
+++ src/velocity.rs
@@ -1,6 +1,13 @@
 use bevy::prelude::*;
 
+#[derive(Resource, Default, Deref, DerefMut)]
+struct IsPaused(bool);
+
 #[derive(Debug, Deref, DerefMut, Default, Component)]
+struct PausedVelocityState(Vec3);
+
+#[derive(Debug, Deref, DerefMut, Default, Component)]
+#[require(PausedVelocityState)]
 pub struct Velocity(pub Vec3);
 
 #[derive(Component)]
@@ -25,10 +32,38 @@ fn draw_arrow(
     }
 }
 
+fn toggle_pause(mut is_paused: ResMut<IsPaused>) {
+    **is_paused = !**is_paused;
+}
+
+fn handle_pause(
+    mut velocities: Query<(&mut Velocity, &mut PausedVelocityState)>,
+    is_paused: Res<IsPaused>,
+) {
+    for (mut velocity, mut paused_state) in &mut velocities {
+        if **is_paused {
+            **paused_state = **velocity;
+            **velocity = Vec3::ZERO;
+        } else {
+            **velocity = **paused_state;
+            **paused_state = Vec3::ZERO;
+        }
+    }
+}
+
+fn pause_condition(keys: Res<ButtonInput<KeyCode>>) -> bool {
+    keys.just_pressed(KeyCode::Enter)
+}
+
 pub struct VelocityPlugin;
 
 impl Plugin for VelocityPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, (_move, draw_arrow).chain());
+        app.add_systems(Update, (_move, draw_arrow).chain())
+            .insert_resource(IsPaused::default())
+            .add_systems(
+                Update,
+                (toggle_pause, handle_pause).chain().run_if(pause_condition),
+            );
     }
 }
