a4ef997fe177d6f35b548fd19aac256693b6e1c8
tonymushah <tonyramanantsalama@gmail.com>
2025-02-06 04:12:14 +0300

    Added random spawns and despawn_all

--- src/birds.rs
+++ src/birds.rs
@@ -1,6 +1,10 @@
 use bevy::prelude::*;
+use rand::{rng, Rng};
 
-use crate::velocity::{ShowVelocityVector, Velocity};
+use crate::{
+    utils::random_translation_uniform,
+    velocity::{ShowVelocityVector, Velocity},
+};
 
 pub mod cube_bound;
 pub mod look_to;
@@ -11,33 +15,52 @@ pub mod shape;
 #[require(Mesh3d, Velocity)]
 pub struct Bird;
 
-fn setup(
+fn spawns(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands.spawn((
-        Bird,
-        Transform::from_translation(Vec3::new(0.0, 1.0, 0.0)),
-        Mesh3d(meshes.add(shape::bird_meshes())),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: Color::Srgba(Srgba::hex("#ffa0d1").unwrap()),
-            ..Default::default()
-        })),
-        Velocity(Vec3::Z * 1.25),
-        ShowVelocityVector,
-    ));
+    let mut _rng = rng();
+    for _ in 1.._rng.random_range(2..10) {
+        commands.spawn((
+            Bird,
+            Transform::from_translation(random_translation_uniform(&mut _rng, -20.0..20.0)),
+            Mesh3d(meshes.add(shape::bird_meshes())),
+            MeshMaterial3d(materials.add(StandardMaterial {
+                base_color: Color::Srgba(Srgba::hex("#ffa0d1").unwrap()),
+                ..Default::default()
+            })),
+            Velocity(random_translation_uniform(&mut _rng, -4.0..5.0)),
+            ShowVelocityVector,
+        ));
+    }
+}
+
+fn spawn_by_key_condition(keys: Res<ButtonInput<KeyCode>>) -> bool {
+    keys.just_pressed(KeyCode::Space)
+}
+
+fn despawn_all(mut commands: Commands, birds: Query<Entity, With<Bird>>) {
+    for bird in &birds {
+        commands.entity(bird).despawn();
+    }
+}
+
+fn despawn_all_by_key_condition(keys: Res<ButtonInput<KeyCode>>) -> bool {
+    keys.just_pressed(KeyCode::F5)
 }
 
 pub struct BirdsPlugin;
 
 impl Plugin for BirdsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup)
+        app.add_systems(Startup, spawns)
             .add_systems(Update, look_to::look_to)
             .add_plugins(random_vel::BirdsRandomVelPlugin)
             .add_plugins(cube_bound::BirdCubeBoundPlugin(Cuboid::new(
                 30.0, 30.0, 30.0,
-            )));
+            )))
+            .add_systems(Update, spawns.run_if(spawn_by_key_condition))
+            .add_systems(Update, despawn_all.run_if(despawn_all_by_key_condition));
     }
 }
