0ceea05033f0db8ead4d064a73f0a0406a2925bb
tonymushah <tonyramanantsalama@gmail.com>
2025-02-06 04:39:26 +0300

    out camera

--- src/camera.rs
+++ src/camera.rs
@@ -13,9 +13,9 @@ fn setup_camera(mut commands: Commands) {
         MainCamera,
         // PanOrbitCamera,
         Transform::from_translation(Vec3 {
-            x: 50.0,
-            y: 50.0,
-            z: 12.0,
+            x: 65.0,
+            y: 44.0,
+            z: 52.0,
         })
         .looking_at(Vec3::ZERO, Vec3::Y),
         PanOrbitCamera::default(),
