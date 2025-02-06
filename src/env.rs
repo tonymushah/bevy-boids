73581f19295075475e1f351882780f54d43dae3d
tonymushah <tonyramanantsalama@gmail.com>
2025-02-06 04:23:19 +0300

    removed plane with light update

--- src/env.rs
+++ src/env.rs
@@ -9,23 +9,18 @@ fn setup(
         Mesh3d(meshes.add(Cuboid::new(30.0, 30.0, 30.0))),
         MeshMaterial3d(materials.add(StandardMaterial {
             base_color: Color::Srgba(Srgba::new(0.8, 0.34, 0.61, 0.2)),
-            alpha_mode: AlphaMode::AlphaToCoverage,
+            alpha_mode: AlphaMode::Add,
             ..Default::default()
         })),
     ));
 
-    commands.spawn((
-        Mesh3d(meshes.add(Plane3d::new(Vec3::Y, Vec2::new(10.0, 10.0)))),
-        MeshMaterial3d(materials.add(StandardMaterial::default())),
-    ));
-    commands.spawn((
-        PointLight {
-            color: Color::Srgba(Srgba::hex("#f0fdf5").unwrap()),
-            range: 30.0,
-            ..Default::default()
-        },
-        Transform::from_translation(Vec3::new(7.0, 7.0, -5.0)),
-    ));
+    commands.spawn((PointLight {
+        color: Color::Srgba(Srgba::hex("#f0fdf5").unwrap()),
+        range: 30.0,
+        intensity: 5_000_000f32,
+        shadows_enabled: true,
+        ..Default::default()
+    },));
 }
 
 pub struct EnvironmentPlugin;
